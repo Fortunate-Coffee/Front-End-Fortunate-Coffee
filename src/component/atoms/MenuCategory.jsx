4ddcb71a870ef1a3e68093adfc69783a8e8db6cb
fndsjy <fendysanjaya40@gmail.com>
2024-03-15 16:35:08 +0700

    Adding the Dinamis Menu Category with useParams()

--- src/component/atoms/MenuCategory.jsx
+++ src/component/atoms/MenuCategory.jsx
@@ -1,27 +1,16 @@
 import { Link, NavLink } from "react-router-dom";
+import { categoryFiles } from "../../menu";
 
 const MenuCategory = () => {
-    const categoryFiles = [
-        {image: "Menu Spesial.png", name: "Menu Spesial"},
-        {image: "Nasi.png", name: "Nasi"},
-        {image: "Menu Paket.png", name: "Menu Paket"},
-        {image: "Kwetiau.png", name: "Kwetiau"},
-        {image: "Mie.png", name: "Mie"},
-        {image: "Bihun.png", name: "Bihun"},
-        {image: "Sop.png", name: "Sop"},
-        {image: "Salad.png", name: "Salad"},
-        {image: "Coffee.png", name: "Coffee"}
-    ];
-
     return(
         <div className="flex flex-wrap">
             {categoryFiles.map((category, index) => (
                 <div key={index} className="w-1/3 sm:w-1/4 md:w-1/9 lg:w-1/6 mb-3">
-                    <NavLink as={Link} to={"/"}>
+                    <NavLink as={Link} to={`/category/${category.slug}`}>
                         <img
                             src={`/images/Menu/${category.image}`}
                             alt="Banner Home"
-                            className="h-auto w-full px-1"
+                            className="h-auto w-auto px-1"
                         />
                         <p className="text-center text-sm mt-2">{category.name}</p>
                     </NavLink>
--- src/component/atoms/SearchingButton.jsx
+++ src/component/atoms/SearchingButton.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const SearchingButton = () => {
     return(
         <div>
-            <Link to={'/'}><i className='fa-solid fa-magnifying-glass fa-beat' style={{color: "#000000;"}}></i></Link>
+            <Link to={'/'}><i className='fa-solid fa-magnifying-glass fa-beat text-black'></i></Link>
         </div>
     )
 }
