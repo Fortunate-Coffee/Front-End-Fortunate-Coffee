3047c78ba9775c7fee3157432ffac0ffdcbd3d88
fndsjy <fendysanjaya40@gmail.com>
2024-04-17 16:29:15 +0700

    Adding Food Ingredients and New Menu Form

--- src/component/templates/AdminMenu.jsx
+++ src/component/templates/AdminMenu.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import AdminCategoriesForm from '../organisms/AdminCategoriesForm';
+import AdminNewMenuForm from '../organisms/AdminNewMenuForm';
 import AdminMenuItems from '../organisms/AdminMenuItems';
+import AdminFoodIngredientsForm from '../organisms/AdminFoodIngredientsForm';
 
 const AdminMenu = () => {
-    const [showCategoriesForm, setShowCategoriesForm] = useState(false); // State untuk form kategori
-    const [showAddMenuForm, setShowAddMenuForm] = useState(false); // State untuk form tambah menu
+    const [showCategoriesForm, setShowCategoriesForm] = useState(false);
+    const [showAddMenuForm, setShowAddMenuForm] = useState(false);
+    const [showFoodIngredientsForm ,setShowFoodIngredientsForm] = useState(false);
 
     const categories = [
         {text: 'Limited Offer'},
@@ -14,47 +18,19 @@ const AdminMenu = () => {
         {text: 'Fortunate Rice'},
     ];
 
-    return(
+    return (
         <div>
-            {/* Form untuk kategori */}
-            {showCategoriesForm && (
-                <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50 z-50">
-                    <div className="bg-white p-4 rounded-xl shadow-xl relative">
-                        {/* Tombol untuk menutup form */}
-                        <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" onClick={() => setShowCategoriesForm(false)}>
-                            <i className="fas fa-times"></i>
-                        </button>
-                        {/* Isi dari form kecil di sini */}
-                        <h2 className="text-lg font-semibold mb-2">Categories Form</h2>
-                        {/* Isi form di sini */}
-                    </div>
-                </div>
-            )}
-
-            {/* Form untuk menambah menu */}
-            {showAddMenuForm && (
-                <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50 z-50">
-                    <div className="bg-white p-4 rounded-xl shadow-xl relative">
-                        {/* Tombol untuk menutup form */}
-                        <button className="absolute top-2 right-2 text-gray-500 hover:text-gray-700" onClick={() => setShowAddMenuForm(false)}>
-                            <i className="fas fa-times"></i>
-                        </button>
-                        {/* Isi dari form kecil di sini */}
-                        <h2 className="text-lg font-semibold mb-2">Add New Menu Form</h2>
-                        {/* Isi form di sini */}
-                    </div>
-                </div>
-            )}
+            {showCategoriesForm && <AdminCategoriesForm setShowCategoriesForm={setShowCategoriesForm} />}
+            {showAddMenuForm && <AdminNewMenuForm setShowAddMenuForm={setShowAddMenuForm} setShowFoodIngredientsForm={setShowFoodIngredientsForm} />}
+            {showFoodIngredientsForm && <AdminFoodIngredientsForm setShowFoodIngredientsForm={setShowFoodIngredientsForm} />}
 
             <div className="p-3 flex justify-between items-center bg-[#43745B] shadow-xl">
                 <h1 className='text-white tracking-wide'>Menu</h1>
                 <div className="flex">
-                    {/* Link untuk menampilkan form kategori */}
                     <Link to="#" onClick={() => setShowCategoriesForm(true)} className='px-3 py-2 flex ms-5 flex-row shadow-2xl rounded-xl bg-white'>
                         <i className="flex items-center fas fa-table fa-lg text-[#43745B]"></i>
                         <p className='ms-2 text-[#43745B]'>Categories</p>
                     </Link>
-                    {/* Link untuk menampilkan form tambah menu */}
                     <Link to="#" onClick={() => setShowAddMenuForm(true)} className='px-3 py-2 flex ms-5 flex-row shadow-2xl rounded-xl bg-white'>
                         <i className="flex items-center fas fa-square-plus fa-lg text-[#43745B]"></i>
                         <p className='ms-2 text-[#43745B]'>Add New Menu</p>
