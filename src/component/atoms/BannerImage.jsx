95ecd3777724d6c2289ad558a8fee7711f312f0e
fndsjy <fendysanjaya40@gmail.com>
2024-05-26 05:27:13 +0700

    Trying Link Image in Deploying

--- src/component/atoms/BannerImage.jsx
+++ src/component/atoms/BannerImage.jsx
@@ -4,7 +4,7 @@ const BannerImage = () => {
     return(
         <div className="mt-24 my-8">
             <NavLink as={Link} to={"/"}>
-                <img src="https://i.ibb.co/wQ6PkyW/Banner.jpg" alt="Banner Home" className="w-full lg:h-72 rounded-xl shadow-xl" />
+                <img src="https://ik.imagekit.io/fndsjy/Fortunate_Coffee/Banner.jpg?updatedAt=1716675763691" alt="Banner Home" className="w-full lg:h-72 rounded-xl shadow-xl" />
             </NavLink>
         </div>
     )
